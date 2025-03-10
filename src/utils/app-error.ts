6adb1e90eee93e664cbccc573ca9dd05be193858
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-10 22:09:30 +0000

    Mar 10, 2025, 11:09 PM

--- src/utils/app-error.ts
+++ src/utils/app-error.ts
@@ -1,10 +1,12 @@
 export class AppError extends Error {
   statusCode: number;
+  isOperation: boolean;
 
   constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
     this.name = this.constructor.name;
+    this.isOperation = true;
     Error.captureStackTrace(this, this.constructor);
   }
 }
