8521e06eabd28225421159f8366456177b1bd4f2
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-10 22:04:07 +0000

    Mar 10, 2025, 11:04 PM

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express, { Response } from "express";
+import express from "express";
 import logger from "./config/logger";
 import { errorHandler, notFoundHandler } from "./middleware/error.middleware";
 import cookieParser from "cookie-parser";
+import routes from "./routes/index";
 
 const app = express();
 
@@ -9,11 +10,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(logger);
 
+app.use("/api", routes);
+
 app.use(notFoundHandler);
 app.use(errorHandler);
-
-app.get("/", (req, res: Response) => {
-  res.json({ message: "Hello, World!" });
-});
 
 export default app;
