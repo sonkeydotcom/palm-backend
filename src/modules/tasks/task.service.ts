935c8040fb59a37e0670dd67f2948d1369873534
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-13 13:16:12 +0000

    Mar 13, 2025, 2:16 PM

--- src/modules/tasks/task.service.ts
+++ src/modules/tasks/task.service.ts
@@ -1,9 +1,18 @@
+import { eq } from "drizzle-orm";
 import db from "../../config/database";
 import { Service, services } from "../services/services.schema";
 
 export class TaskService {
+  // Get all tasks from the database
   async getAllTasks(): Promise<Service[]> {
     return db.select().from(services);
+  }
+
+  // Get a task by its ID
+
+  async getTaskById(id: number): Promise<Service | null> {
+    const result = await db.select().from(services).where(eq(services.id, id));
+    return result[0];
   }
 }
 
