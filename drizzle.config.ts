b162023e66fd064965b92dd03ca3a9ee145feed0
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-13 09:43:39 +0000

    Mar 13, 2025, 10:43 AM

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+import { DATABASE_URL } from "./src/config/env";
+
+export default defineConfig({
+  out: "./drizzle",
+  schema: [
+    "./src/modules/**/**/*.schema.ts", // Include schemas from modular folders
+  ],
+  dialect: "postgresql",
+  dbCredentials: {
+    url: DATABASE_URL!,
+  },
+});
