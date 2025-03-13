138f0dfd06f5c8d125e65e3abc85ed554c6f3d52
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-13 13:15:10 +0000

    Mar 13, 2025, 2:15 PM

--- src/modules/tasks/task.service.ts
+++ src/modules/tasks/task.service.ts
@@ -1,3 +1,10 @@
-export class TaskService {}
+import db from "../../config/database";
+import { Service, services } from "../services/services.schema";
+
+export class TaskService {
+  async getAllTasks(): Promise<Service[]> {
+    return db.select().from(services);
+  }
+}
 
 export const taskService = new TaskService();
