a7cbc9e76ddfc429e6b7343432d494d345d359a1
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-16 03:12:55 +0000

    Mar 16, 2025, 4:12 AM

--- src/modules/bookings/booking.service.ts
+++ src/modules/bookings/booking.service.ts
@@ -1,5 +1,6 @@
+import { eq } from "drizzle-orm";
 import db from "../../config/database";
-import { Booking, bookings } from "./booking.schema";
+import { Booking, bookings, NewBooking } from "./booking.schema";
 
 export class BookingService {
   async findAll(): Promise<Booking[]> {
@@ -9,6 +10,39 @@ export class BookingService {
   async findById(id: number): Promise<Booking | null> {
     const result = await db.select().from(bookings).where(eq(bookings.id, id));
     return result[0];
+  }
+
+  async create(bookingData: Omit<NewBooking, "id">): Promise<Booking> {
+    const result = await db
+      .insert(bookings)
+      .values({ ...bookingData })
+      .returning();
+
+    return result[0];
+  }
+
+  async update(
+    id: number,
+    bookingData: Partial<Booking>
+  ): Promise<Booking | null> {
+    const result = await db
+      .update(bookings)
+      .set({
+        ...bookingData,
+        updatedAt: new Date(),
+      })
+      .where(eq(bookings.id, id))
+      .returning();
+
+    return result[0];
+  }
+
+  async delete(id: number): Promise<boolean> {
+    const result = await db
+      .delete(bookings)
+      .where(eq(bookings.id, id))
+      .returning();
+    return result.length > 0;
   }
 }
 
