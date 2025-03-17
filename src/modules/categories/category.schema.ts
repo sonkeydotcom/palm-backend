594aaf5a5cedfd5b76555772af4922bfc452bee0
sonkeydotcom <annagu.kennedy@gmail.com>
2025-03-17 02:27:41 +0000

    Mar 17, 2025, 3:27 AM

---
+++ src/modules/categories/category.schema.ts
@@ -0,0 +1,48 @@
+import {
+  InferInsertModel,
+  InferSelectModel,
+  relations,
+  sql,
+} from "drizzle-orm";
+import {
+  pgTable,
+  serial,
+  varchar,
+  text,
+  timestamp,
+  boolean,
+} from "drizzle-orm/pg-core";
+import { tasks } from "../tasks/task.schema";
+
+export const services = pgTable("services", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 100 }).notNull(),
+  description: text("description"),
+  icon: text("icon"),
+  slug: varchar("slug", { length: 100 }).notNull().unique(),
+  parentId: serial("parent_id"),
+  displayOrder: serial("display_order"),
+  isActive: boolean("is_active").default(true).notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const subservices = pgTable("subservices", {
+  id: serial("id").primaryKey(),
+  serviceId: serial("service_id").references(() => services.id),
+  name: varchar("name", { length: 100 }).unique().notNull(),
+  description: varchar("description", { length: 255 }),
+  keywords: text("keywords"),
+  createdAt: timestamp("created_at").default(sql`now()`),
+  updatedAt: timestamp("updated_at").default(sql`now()`),
+});
+
+export const serviceRelations = relations(services, ({ many }) => ({
+  subservices: many(subservices),
+  tasks: many(tasks),
+}));
+
+export type Service = InferSelectModel<typeof services>;
+export type Subcservice = InferSelectModel<typeof subservices>;
+export type NewService = InferInsertModel<typeof services>;
+export type NewSubservice = InferInsertModel<typeof subservices>;
