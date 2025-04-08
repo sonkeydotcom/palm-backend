5a753a4809f488c25bd2ca450820e4b221b7a127
sonkiee <annagu.kennedy@gmail.com>
2025-04-08 13:26:23 +0000

    Apr 8, 2025, 2:26 PM

--- src/modules/payments/payment.service.ts
+++ src/modules/payments/payment.service.ts
@@ -1,5 +1,11 @@
+import db from "../../config/database";
+import { NewPayment, payments } from "./payment.schema";
+
 export class PaymentService {
-  async initiatePayment() {}
+  async initiatePayment(data: NewPayment) {
+    const results = await db.insert(payments).values(data).returning();
+    return results[0];
+  }
 }
 
 export const paymentService = new PaymentService();
