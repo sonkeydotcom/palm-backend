944fa16cc9cf2af9a5f3ceba25951b910421f4f8
sonkiee <annagu.kennedy@gmail.com>
2025-04-08 13:17:20 +0000

    Apr 8, 2025, 2:17 PM

---
+++ src/modules/payments/index.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { SQUADCO_API_URL, SQUADCO_PUBLIC_KEY } from "../../config/env";
+
+export const squadApi = axios.create({
+  baseURL: SQUADCO_API_URL,
+  headers: {
+    Authorization: `Bearer ${SQUADCO_PUBLIC_KEY}`,
+  },
+});
